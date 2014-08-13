b185cbf3d9adb9577ebad3f164e754c686a00eb1
Graham Abbott <graham.abbott@gmail.com>
2014-08-13 11:21:56 -0700

    Fixed a bug that resulted in the extension only working on the first window for a profile, now works on the currently focused window regardless. Changed the behaviour for the extension button as well, now opens a config page (does not currently work).

--- background.js
+++ background.js
@@ -6,39 +6,57 @@ var pins = [
     "https://www.google.com/calendar/",
     "https://app.asana.com/",
     "https://trello.com/",
-    "https://dayboard.co/home",
+    "https://dayboard.co/",
     "https://github.com/graham"
 ];
+
+function get_current_window_id(cb) {
+    chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
+        if (tab) {
+            cb(tab[0].windowId);
+        }
+    })
+}
+
+chrome.browserAction.onClicked.addListener( function(activeTab) {
+    var newURL = chrome.extension.getURL("/config.html");
+    chrome.tabs.create({ url: newURL });
+});
 
 (function() {
 	chrome.commands.onCommand.addListener(function(command) {
         if (command == "cleanup-and-reset") {
-            chrome.tabs.query({}, function(tabs) {
-                var to_open = [];
+            get_current_window_id(function(current_win_id) {
+                chrome.tabs.query({windowId:current_win_id}, function(tabs) {
+                    var to_open = [];
 
-                for( var i = 0; i < pins.length; i++ ) {
-                    var hit = false;
-
-                    for( var j = 0; j < tabs.length; j++ ) {
-                        var tab = tabs[j].url;
-                        if (tab.slice(0, pins[i].length) == pins[i] &&
-                            tabs[j].pinned == true) {
-                            hit = true;
-                            chrome.tabs.move(tabs[j].id, {index:i}, function() {});
-                        }
+                    for( var i = 0; i < pins.length; i++ ) {
+                        var hit = false;
+                        
+                        for( var j = 0; j < tabs.length; j++ ) {
+                            var tab = tabs[j].url;
                             
+                            console.log('tab: ' + tabs[j].windowId + ":" + tabs[j].index);
+                            
+                            if (tab.slice(0, pins[i].length) == pins[i] &&
+                                tabs[j].pinned == true) {
+                                hit = true;
+                                chrome.tabs.move(tabs[j].id, {index:i}, function() {});
+                            }
+                            
+                        }
+                        if (hit == false) {
+                            to_open.push(pins[i]);
+                        }
                     }
-                    if (hit == false) {
-                        to_open.push(pins[i]);
+                    
+                    for( var i = 0; i < to_open.length; i++ ) {
+                        var url = to_open[i];
+                        chrome.tabs.create({ active:false, url: url, pinned:true, windowId:current_win_id });
                     }
-                }
-
-                for( var i = 0; i < to_open.length; i++ ) {
-                    var url = to_open[i];
-                    chrome.tabs.create({ active:false, url: url, pinned:true });
-                }
-
-                console.log("Received the command.");
+                    
+                    console.log("Received the command.");
+                });
             });
         }
 	});
