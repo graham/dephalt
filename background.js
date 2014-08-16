d0f9b0a53595ec2345cd3f7a550d5e4f71c930e1
Graham Abbott <graham.abbott@gmail.com>
2014-08-16 15:22:28 -0700

    added a config menu as well as some better defaults.

--- background.js
+++ background.js
@@ -1,14 +1,35 @@
 var last = null;
+var storage = localStorage;
 
-var pins = [
+var defaults = [
     "https://pulled.site44.com/",
     "https://mail.google.com/",
     "https://www.google.com/calendar/",
-    "https://app.asana.com/",
-    "https://trello.com/",
-    "https://dayboard.co/",
+    "https://www.dropbox.com/photos",
     "https://github.com/graham"
 ];
+
+function get_pins() {
+    if (storage.getItem('dephalt_pins') == undefined) {
+        set_pins(defaults);
+        return defaults;
+    } else {
+        try {
+            return JSON.parse(storage.getItem('dephalt_pins'));
+        } catch (err) {
+            set_pins(defaults);
+            return defaults;
+        }
+    }
+}
+
+function set_pins(s) {
+    try {
+        storage.setItem('dephalt_pins', JSON.stringify(s));
+    } catch (err) {
+        storage.setItem('dephalt_pins', JSON.stringify(defaults));
+    }
+}
 
 function get_current_window_id(cb) {
     chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
@@ -17,6 +38,8 @@ function get_current_window_id(cb) {
         }
     })
 }
+
+var str_trim = function(s) { return s.replace(/^\s+|\s+$/g, "").replace(/^[\n|\r]+|[\n|\r]+$/g, ""); };
 
 chrome.browserAction.onClicked.addListener( function(activeTab) {
     var newURL = chrome.extension.getURL("/config.html");
@@ -29,21 +52,16 @@ chrome.browserAction.onClicked.addListener( function(activeTab) {
             get_current_window_id(function(current_win_id) {
                 chrome.tabs.query({windowId:current_win_id}, function(tabs) {
                     var to_open = [];
-
+                    var pins = get_pins();
                     for( var i = 0; i < pins.length; i++ ) {
                         var hit = false;
-                        
                         for( var j = 0; j < tabs.length; j++ ) {
                             var tab = tabs[j].url;
-                            
-                            console.log('tab: ' + tabs[j].windowId + ":" + tabs[j].index);
-                            
                             if (tab.slice(0, pins[i].length) == pins[i] &&
                                 tabs[j].pinned == true) {
                                 hit = true;
                                 chrome.tabs.move(tabs[j].id, {index:i}, function() {});
                             }
-                            
                         }
                         if (hit == false) {
                             to_open.push(pins[i]);
@@ -61,3 +79,18 @@ chrome.browserAction.onClicked.addListener( function(activeTab) {
         }
 	});
 })();
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      if (request.type == "save") {
+          var pins = request.content.split('\n');
+          for( var i = 0; i < pins.length; i++ ) {
+              pins[i] = str_trim(pins[i]);
+          }
+          set_pins(pins);
+          sendResponse({response:"done", result:pins});
+      } else if (request.type == "get") {
+          sendResponse({response:get_pins()});
+      }
+  });
